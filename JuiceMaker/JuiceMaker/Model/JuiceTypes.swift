e184db14b4fd9d0c2997dae78e779a659a73ea7d
Eddy <kimkyunghun3@naver.com>
2022-04-05 14:25:09 +0900

    feat: 각 쥬스에 따라 과일 소모되는 레시피 기능 구현

--- JuiceMaker/JuiceMaker/Model/JuiceTypes.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceTypes.swift
@@ -9,4 +9,23 @@ import Foundation
 
 enum JuiceTypes {
     case strawberryJuice, bananaJuice, kiwiJuice, pineappleJuice, strawberryBananaJuice, mangoJuice, mangoKiwiJuice
+    
+    func recipe() -> [FruitTypes:Int] {
+        switch self {
+        case .strawberryJuice:
+            return [FruitTypes.starwberry: 16]
+        case .bananaJuice:
+            return [FruitTypes.banana: 2]
+        case .kiwiJuice:
+            return [FruitTypes.kiwi: 3]
+        case .pineappleJuice:
+            return [FruitTypes.pineapple: 2]
+        case .strawberryBananaJuice:
+            return [FruitTypes.starwberry: 10, FruitTypes.banana: 1]
+        case .mangoJuice:
+            return [FruitTypes.mango: 3]
+        case .mangoKiwiJuice:
+            return [FruitTypes.mango: 2, FruitTypes.kiwi: 1]
+        }
+    }
 }
