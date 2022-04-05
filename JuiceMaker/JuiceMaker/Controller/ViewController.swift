c6bef71e270d43fccc6819f77b6f65a47ab95022
Eddy <kimkyunghun3@naver.com>
2022-04-05 22:30:05 +0900

    feat: 과일 재고에 따른 얼럿 메시지 및 뷰컨트롤러간 segue 연결 구현

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -11,5 +11,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func touchedChangeStockButton(_ sender: Any) {
+        self.performSegue(withIdentifier: "showFruitsStockSegue", sender: self)
+    }
+    
+    
+    func showJuiceAlert() {
+        let alert = UIAlertController(title: "성공", message: "쥬스 제조 후 쥬스 나왔습니다! 맛있게 드세요!", preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "닫기", style: .default)
+        alert.addAction(okButton)
+        present(alert, animated: true)
+    }
+    
+    func showOutOfStockAlert() {
+        let alert = UIAlertController(title: "재고 부족!", message: "재료가 모자라요. 재고를 수정할까요?", preferredStyle: .alert)
+        let noButton = UIAlertAction(title: "아니오", style: .default)
+        let yesButton = UIAlertAction(title: "예", style: .default) { (action) in
+            // 다음 화면으로 전환
+        }
+        
+        alert.addAction(yesButton)
+        alert.addAction(noButton)
+        present(alert, animated: true)
+    }
 }
 
