967994e9a214d272a348b13f878834c6d0756912
Eddy <kimkyunghun3@naver.com>
2022-04-05 15:04:53 +0900

    feat: 과일 모든 재고 10으로 초기화 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,11 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    var Fruits = [FruitTypes: Int]()
     
+    init() {
+        for fruit in Fruits {
+            Fruits[fruit.key] = 10
+        }
+    }
 }
