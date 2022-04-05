f5b5d55502d64f762f7fd316227912e46d72fe62
Eddy <kimkyunghun3@naver.com>
2022-04-05 14:11:26 +0900

    feat: FruitTypes, JuiceTypes 열거형 선언

---
+++ JuiceMaker/JuiceMaker/Model/FruitTypes.swift
@@ -0,0 +1,10 @@
+//
+//  FruitTypes.swift
+//  JuiceMaker
+//
+//  Created by Eddy on 2022/04/05.
+//
+
+enum FruitTypes {
+    case starwberry, banana, kiwi, pineapple, mango
+}
---
+++ JuiceMaker/JuiceMaker/Model/JuiceTypes.swift
@@ -0,0 +1,12 @@
+//
+//  JuiceTypes.swift
+//  JuiceMaker
+//
+//  Created by Eddy on 2022/04/05.
+//
+
+import Foundation
+
+enum JuiceTypes {
+    case strawberryJuice, bananaJuice, kiwiJuice, pineappleJuice, strawberryBananaJuice, mangoJuice, mangoKiwiJuice
+}
