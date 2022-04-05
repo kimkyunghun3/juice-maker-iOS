43227767c6277e58c76738affa4b561c24736897
Eddy <kimkyunghun3@naver.com>
2022-04-05 21:56:56 +0900

    feat: juice 만드는 메서드 구현

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,14 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
-    
+
+    func makeJuice(juice: JuiceTypes, fruitStore: FruitStore) -> [FruitTypes:Int] {
+        var fruitStock = [FruitTypes:Int]()
+        do {
+            fruitStock = try fruitStore.consumeFruit(recipe: juice.recipe())
+        } catch JuiceMakerError.outOfStock {
+        } catch {
+        }
+        return fruitStock
+    }
 }
